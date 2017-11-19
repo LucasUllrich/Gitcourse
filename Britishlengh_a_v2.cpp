02c4223fa7911a874b242316ddd0247a9e000ab3
LucasUllrich <lucas.ullrich@aon.at>
2017-11-19 15:26:36 +0100

    Add files via upload

--- Britishlengh_a_v2.cpp
+++ Britishlengh_a_v2.cpp
@@ -3,40 +3,66 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define DEBUG 1
+
 class BritishLength {
 	float inch;
 	
 	public:
-	
-	
+	int get_foot();
+	float get_inch();
+	float get_inch_part();
+	BritishLength();
+	BritishLength(int ft);
+	BritishLength(int ft,float in);
 };
 
 BritishLength::BritishLength() {
 	inch = 0;
+	#if DEBUG
+	printf("inch: %f\n", inch);
+	#endif
 }
 
 BritishLength::BritishLength(int ft) {
 	inch = ft * 12;
+	#if DEBUG
+	printf("inch: %f\n", inch);
+	#endif
 }
 
 BritishLength::BritishLength(int ft, float in) {
 	inch = ft * 12 + in;
+	#if DEBUG
+	printf("inch: %f\n", inch);
+	#endif
 }
 
+int BritishLength::get_foot() {
+	int ft;
+	ft = inch / 12;
+	return ft;
+}
 
+float BritishLength::get_inch() {
+	float in;
+	in = inch;
+	return in;
+}
 
-
+float BritishLength::get_inch_part() {
+	float in;
+	in = inch % 12;
+	return in;
+}
 
 int main() {
-	BritishLength b;
-	int x;
-	
-	printf("neu..............0\n"
-		   "add.........1\n"
-		   "sub.....2\n"
-		   "mult...3\n"
-		   "div.......4\n");
-	scanf("%d", &x);
+	BritishLength b();
+	int ft;
+	float in;
+	ft = b.get_foot();
+	in = b.get_inch();
+	in = b.get_inch_part();
 	
 	return 0;
 }
